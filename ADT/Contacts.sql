3da917e88f5d8c9bd0ed532545660e6a7beb0ea7
LarryKDC <LarryKDC@users.noreply.github.com>
2016-08-03 15:07:25 -0400

    updated for 16-17 school year

--- ADT/Contacts.sql
+++ ADT/Contacts.sql
@@ -3,37 +3,22 @@ SELECT
 , '2015-2016' AS SCHOOL_YEAR 
 , TO_CHAR(SYSDATE,'YYYY-MM-DD HH24:MI') AS COLLECTION_DATE
 , 'Contacts' AS COLLECTION_TYPE
-, TO_CHAR(s.id) AS SIS_ID 
-, '' AS CONTACT_ID 
-, '' AS CONTACT_TYPE
-, '' AS PRIMARY_GUARDIAN_INDICATOR 
-, CASE
-    WHEN REGEXP_COUNT(s.mother,'[ ,]') = 0 THEN s.mother
-    ELSE
-      CASE
-        WHEN REGEXP_COUNT(s.mother,',') = 0 THEN SUBSTR(s.mother,REGEXP_INSTR(s.mother,' ',1,REGEXP_COUNT(s.mother,' ')) + 1)
-        ELSE SUBSTR(s.mother,1,REGEXP_INSTR(s.mother,',',1,REGEXP_COUNT(s.mother,','))-1)
-      END
-  END CONTACT_LAST_NAME
-, CASE
-    WHEN REGEXP_COUNT(s.mother,'[ ,]') = 0 THEN NULL
-    ELSE
-      CASE
-        WHEN REGEXP_COUNT(s.mother,',') = 0 THEN SUBSTR(s.mother,1,REGEXP_INSTR(s.mother,' ',1,REGEXP_COUNT(s.mother,' ')) - 1)
-        ELSE SUBSTR(s.mother,REGEXP_INSTR(s.mother,',',1,REGEXP_COUNT(s.mother,',')) + 2)
-      END
-  END CONTACT_FIRST_NAME
-, NULL CONTACT_MIDDLE_NAME
-, '' AS CONTACT_PHONE_TYPE
-, CASE WHEN (COALESCE(ps_customfields.getStudentsCF(s.id,'motherdayphone'),s.home_phone)) IS NOT NULL THEN 
-  '('||SUBSTR(COALESCE(ps_customfields.getStudentsCF(s.id,'motherdayphone'),s.home_phone),0,3)||') '||SUBSTR(COALESCE(ps_customfields.getStudentsCF(s.id,'motherdayphone'),s.home_phone),5)
-  END CONTACT_PHONE_NUMBER
-, ps_customfields.getStudentsCF(s.id,'Mother_Email') CONTACT_EMAIL_ADDRESS 
-,'' AS ADD_TYPE 
-, s.street ADD_STREET
-, NULL ADD_APT
-, NULL ADD_BLDG
-, s.city ADD_CITY
-, s.state ADD_STATE
-, s.zip ADD_ZIP
+, TO_CHAR(s.student_number) AS SIS_ID
+, '' AS CONTACT_ID
+, case when mother is not null then 111 else 121 end AS CONTACT_TYPE
+, case when ps_customfields.getStudentsCF(s.id,'guardian')='Mother' then 'Y' else 'N' end  AS PRIMARY_GUARDIAN_INDICATOR 
+, substr(mother,instr(mother,' ')+1) as CONTACT_FIRST_NAME
+, '' AS CONTACT_MIDDLE_NAME
+, substr(mother,0,instr(mother,' ')-2) AS CONTACT_LAST_NAME
+, '' AS PHONE_NUMBER_TYPE
+, s.home_phone AS PHONE_NUMBER
+, '' AS EMAIL_ADDRESS
+, case when ps_customfields.getStudentsCF(s.id,'mother_street') is not null then 'MAIL' else null end AS ADDRESS_TYPE
+, s.street AS STREET_ADDRESS
+, '' AS APARTMENT_NUMBER
+, '' AS BUILDING_SITE_NUMBER
+, s.city  AS CITY
+, s.state as STATE
+, s.zip AS ZIP_CODE
 from students s
+where s.entrydate > = '01-JUL-16'
