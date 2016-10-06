2c3b529e159481fc8ab212e3a899046c5088720c
LarryKDC <LarryKDC@users.noreply.github.com>
2016-10-06 15:34:49 -0400

    Update Attendance.sql

--- ADT/Attendance.sql
+++ ADT/Attendance.sql
@@ -7,7 +7,9 @@ select
 , TO_CHAR (cd.date_value, 'MM/DD/YYYY') as ATTENDANCE_DATE
 , a.ATT_MODE_CODE AS ATTENDANCE_COLLECTION_TYPE
 , ac.att_code as ATTENDANCE_STATUS_CODE
-, '' AS ATTENDANCE_ABSENCE_REASON 
+, '' AS ATTENDANCE_ABSENCE_REASON
+, e.entrydate
+, e.exitdate
 FROM students s
 JOIN (
   SELECT id studentid, schoolid, grade_level, entrydate, entrycode, exitdate, exitcode FROM students
@@ -20,3 +22,4 @@ left outer join attendance a on a.att_date = cd.date_value and a.studentid = e.s
 left outer join attendance_code ac on ac.id = a.attendance_codeid
 where cd.date_value between '08-AUG-16' and sysdate
 and cd.insession = 1
+and (ac.att_code != 'X' OR ac.att_code is null) -- Do not send the X code because they correspond to non-school days; using only ac.att_code != 'X' excludes null values, so they must be explicitly inclduded
