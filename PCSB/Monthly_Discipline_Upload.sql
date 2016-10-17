8b4452ea133f6076ef36084e846bd386e8a543c5
LarryKDC <LarryKDC@users.noreply.github.com>
2016-10-17 13:13:31 -0400

    Update Monthly_Discipline_Upload.sql

--- PCSB/Monthly_Discipline_Upload.sql
+++ PCSB/Monthly_Discipline_Upload.sql
@@ -1,105 +1,80 @@
 SELECT
-i.studentschoolid 'Student_Number - Remove',
-case	when	I.schoolid=82	then	'KEY'
-	when	I.schoolid=81	then	'AIM'
-	when	I.schoolid=85	then	'WILL'
-	when	I.schoolid=73	then	'LEAP'
-	when	I.schoolid=77	then	'Promise'
-	when	I.schoolid=71	then	'Discover'
-	when	I.schoolid=72	then	'Grow'
-	when	I.schoolid=75	then	'Heights'
-	when	I.schoolid=76	then	'Lead'
-	when	I.schoolid=79	then	'Spring'
-	when	I.schoolid=70	then	'Connect'
-	when	I.schoolid=83	then	'NE'
-	when	I.schoolid=74	then	'ATA'
-	when	I.schoolid=84	then	'Valor'
-	when	I.schoolid=78	then	'Quest'
-	when	I.schoolid=86	then	'LC'
-	when	I.schoolid=80	then	'KCP'
-	else	null	end 	'School (Incident) - Remove',		
-s.enrollmentstatus 'Enrollment Status - Remove',
-link 'DeansList URL - Remove',
-SC.ABBREVIATION 'School',
-[STATESTUDENTID] 'USI ID',
-LASTNAME 'STUDENT LAST NAME',
-FIRSTNAME 'STUDENT FIRST NAME',
+'Disciplinary' AS 'Collection Type',
+'2016-2017' AS 'School Year',
+'129' AS 'LEA ID',
+'KIPP DC' AS 'LEA',
+ALTERNATE_SCHOOL_NUMBER AS 'Campus ID' , --check that these are the actual OSSE school codes
+SC.NAME AS 'Campus', --Optional
+S.STATESTUDENTID AS 'USI',
+S.SYSTEMSTUDENTID AS 'LEA Student ID',
+CONVERT(VARCHAR,I.ISSUETS,101) AS 'Incident Date',
+NULL AS 'Incident Time', --Optional
+NULL AS 'Incident Location', --Optional
+NULL AS 'Incident Time Description', --Optional
 CASE 
-	WHEN CS.[SPED_CLASSIFICATION] IS NOT NULL THEN 'Y'
-	ELSE 'N' 
-END 'STUDENT HAS AN IEP?',
+	WHEN INFRACTION = 'Alcohol Related' THEN 'Alcohol possession or use'
+	WHEN INFRACTION = 'Sexual Misconduct or Harrassment' THEN 'Harrassment, Sexual'
+	WHEN INFRACTION = 'Theft' THEN 'Robbery'
+	WHEN INFRACTION = 'Threatening Physical Harm' THEN 'Threat/intimidation'
+	WHEN INFRACTION LIKE 'Weapons%' THEN 'Weapons Possession'
+	WHEN INFRACTION IS NULL THEN '**MISSING INFRACTION**'
+	ELSE INFRACTION 
+END AS 'Reason Student Was Disciplined', --transform to match 
+NULL AS 'Secondary Incident Behavior', --Optional
+CASE 
+	WHEN INFRACTION IN ('Bullying','Fighting',
+						'Sexual Misconduct or Harrassment',
+						'Theft','Threatening Physical Harm',
+						'Violent Incident (WITH physical injury) (VIOWINJ)') THEN '**Choose Minor Injury, Serious Bodily Injury or No Injury**'
+END AS 'Injury Type',
 CASE
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Autism','AUT') then 'AUT - Autism'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Deaf-Blindness','DB') then 'DB - Deaf-Blindness'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Developmental Delay','DD') then 'DD - Developmental Delay'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Emotional Disturbance','EMN') then 'EMN - Emotional Disturbance'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Hearing Impaired','HI') then 'HI - Hearing Impairment'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Mental Retardation','MR') then 'MR - Mental Retardation'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Multiple Disabilities','MD') then 'MD - Multiple Disabilities'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Othopedic Impairment','OI') then 'OI - Orthopedic Impairment'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Specific Learning Disability','SLD') then 'SLD - Specific Learning Disability'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Speech or Language Impairment','SLI') then 'SLI - Speech or Language Impairment'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Traumatic Brain Injury','TBI') then 'TBI - Traumatic Brain Injury'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Visual Impairment','VI') then 'VI - Visual Impairment'
-	WHEN CS.[SPED_CLASSIFICATION] IN ('Other Health Impairment','OHI') then 'OHI - Other Health Impairment'
-	ELSE NULL
-END 'PRIMARY DISABILITY',
-'NO' 'WAS STUDENT REMOVED...',
-NULL 'TYPE OF INTERIM REMOVAL',
-NULL 'DATE INTERIM REMOVAL BEGAIN',
-NULL 'LENGTH OF INTERIM REMOVAL (DAYS)',
-NULL 'INTERIM REMOVAL REASON',
-'YES' 'WAS STUDENT SUSPENDED OR EXPELLED',
+	WHEN INFRACTION LIKE 'Weapon%' THEN '**Choose weapon type**'
+	ELSE NULL 
+END AS 'Weapon Type',
 CASE 
-	WHEN P.PENALTYNAME LIKE 'OSS%' THEN 'OUT OF SCHOOL SUSPENSION'
-	--WHEN P.PENALTYNAME LIKE 'ISS%' THEN 'IN SCHOOL SUSPENSION'  --Don't need to report ISS
-	WHEN P.PENALTYNAME = 'Expulsion' THEN 'EXPULSION'
+	WHEN P.PENALTYNAME = 'OSS' THEN 'Out-of-school Suspension'
+	--WHEN P.PENALTYNAME = 'ISS' THEN 'In-school Suspension'
+	ELSE P.PENALTYNAME
+END AS 'Disciplinary Action Taken',
+NULL AS 'Referral to Law Enforcement', --Optional
+NULL AS 'School Related Arrest', --Optional
+P.NUMDAYS AS 'Duration of Disciplinary Action',
+CONVERT(VARCHAR, P.STARTDATE, 101) AS 'Start Date of Disciplinary Action',
+CONVERT(VARCHAR, P.ENDDATE, 101) AS 'End Date of Disciplinary Action',
+NULL AS 'Disciplinary Action Partial Days', --Optional
+'**Choose Yes or No**' AS 'Education Services Received?',
+CASE 
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose Y or N**'
 	ELSE NULL
-END 'TYPE OF SUSPENSION/EXPULSION',
-CONVERT(char(10),P.STARTDATE,101) 'DATE OF SUSPENSION/EXPLUSION', --change date format from yyyy-mm-dd to mm/dd/yyyy
-CONVERT(char(10),P.ENDDATE,101) 'END DATE OF SUSPENSION/EXPLUSION -- REMOVE', --change date format from yyyy-mm-dd to mm/dd/yyyy
-P.NUMDAYS 'REMOVAL LENGTH IN DAYS',
+END AS 'Interim Removal',
+CASE 
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose Weapons, Drugs, or Serious bodily injury**'
+	ELSE NULL
+END AS 'Interim Removal Reason (IDEA)',
 CASE
-	WHEN	I.INFRACTION 	=	'Alcohol Related (A)'	then 	'A - Alcohol Related'
-	WHEN	I.INFRACTION 	=	'Illicit Drug Related (D)'	then 	'D - Illicit Drug Related'
-	WHEN	I.INFRACTION 	=	'Violent Incident (WITH physical injury) (VIOWINJ)'	then 	'VIOWINJ - Violent Incident (with physical injury)'
-	WHEN	I.INFRACTION 	=	'Violent Incident (WITHOUT physical injury) (VIOWOINJ)'	then 	'VIOWOINJ - Violent Incident (without physical injury)'
-	WHEN	I.INFRACTION 	=	'Weapons Possession - Non-firearm (W)'	then 	'W - Weapons Possession'
-	WHEN	I.INFRACTION 	=	'Weapons Possession - Handguns (W-HANDGUNS)'	then 	'W - Weapons Possession (HANDGUNS - Handguns)'
-	WHEN	I.INFRACTION 	=	'Weapons Possession - Use of more than one of the above (W-MULTIPLE)'	then 	'W - Weapons Possession (MULTIPLE - Use of more than one of the above (handguns, rifles/shotgun, or other)'
-	WHEN	I.INFRACTION 	=	'Weapons Possesssion - Any firearm that is not handgun, rifle, or shotgun (W-OTHER)'	then 	'W - Weapons Possession (OTHER - Any firearm that is not a handgun or a rifle or a shotgun)'
-	WHEN	I.INFRACTION 	=	'Weapons Possession - Rifles / Shotgun (W-RIFLESHOTGUN)'	then 	'W - Weapons Possession (RIFLESHOTGUN - Rifles / Shotguns)'
-	WHEN	I.INFRACTION IS NULL THEN NULL
-	ELSE	'OTHER CHARTER- Non-violent violation of school''s discipline or compulsory attendance policy'
-END 'REASON WHY STUDENT WAS DISCIPLINED',
-CASE 
-	WHEN I.INFRACTION IN (	'Academic Dishonesty',
-							'Arson',
-							'Attendance/Skipping/Tardy',
-							'Bullying',
-							'Disruptive Behavior',
-							'Fighting',
-							'Flammables',
-							'Gambling',
-							'Improper Use of Technology',
-							'Insubordination',
-							'Sexual Misconduct or Harassment',
-							'Theft',
-							'Threatening Physical Harm',
-							'Vandalism'
-							) THEN I.INFRACTION
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose REMHO OR REMODW**'
 	ELSE NULL
-END 'OTHER CHARTER REASON',
-NULL 'EDUCATION SERVICES RECIEVED DURING SUSPENSION/EXPULSION'
+END AS 'Type of Interim Removal (IDEA)',	
+CASE
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose Number of Days**'
+	ELSE NULL
+END AS 'Length of Interim Removal (Days)',
+CASE
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose Date as MM/DD/YYYY**'
+	ELSE NULL
+END AS 'Date Interim Removal Began',
+CASE
+	WHEN CS.SPED_FUNDING IS NOT NULL THEN '**Choose Date as MM/DD/YYYY**'
+	ELSE NULL
+END AS 'Date Interim Removal Ended'	
 FROM [DW].[DW_DIMSTUDENT] S
 JOIN CUSTOM.CUSTOM_STUDENTS CS ON CS.SYSTEMSTUDENTID = S.SYSTEMSTUDENTID
 JOIN [CUSTOM].[CUSTOM_STUDENTBRIDGE] SB ON SB.SYSTEMSTUDENTID = S.SYSTEMSTUDENTID
-JOIN [custom].[custom_dlincidents_raw] i on i.studentschoolid = sb.student_number
-JOIN [CUSTOM].[CUSTOM_DLPENALTIES_RAW] P ON p.incidentid = i.incidentid
+JOIN [CUSTOM].[CUSTOM_DLINCIDENTS_RAW] I ON I.STUDENTSCHOOLID = SB.STUDENT_NUMBER
+JOIN [CUSTOM].[CUSTOM_DLPENALTIES_RAW] P ON P.INCIDENTID = I.INCIDENTID
 JOIN POWERSCHOOL.POWERSCHOOL_STUDENTS PS ON PS.STUDENT_NUMBER = SB.STUDENT_NUMBER
 JOIN POWERSCHOOL.POWERSCHOOL_SCHOOLS SC ON SC.SCHOOL_NUMBER = PS.SCHOOLID
 WHERE 1=1
---AND i.issueTS between '2015-10-01' and '2015-10-30'
-AND coalesce(p.startdate,i.issueTS) BETWEEN '2016-05-01' and '2016-05-31'--CONVERT(DATE,DATEADD(MM, DATEDIFF(MM, 0, GETDATE())-2, 0)) AND CONVERT(DATE,DATEADD(MS, -3, DATEADD(MM, DATEDIFF(MM, 0, GETDATE()) , 0)))
-AND P.PENALTYNAME IN ('OSS','Expulsion')--,'ISS Short Term','ISS Long Term')
+AND coalesce(p.startdate,i.issueTS) BETWEEN '2016-08-01' and '2016-09-30'--CONVERT(DATE,DATEADD(MM, DATEDIFF(MM, 0, GETDATE())-2, 0)) AND CONVERT(DATE,DATEADD(MS, -3, DATEADD(MM, DATEDIFF(MM, 0, GETDATE()) , 0)))
+AND P.PENALTYNAME IN ('OSS','Expulsion')--,'ISS')
 order by p.startdate
