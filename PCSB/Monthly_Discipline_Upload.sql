b6f53f3869cc862f6637481ce32281c9a3c64195
LarryKDC <LarryKDC@users.noreply.github.com>
2017-02-09 11:28:06 -0500

    Update Monthly_Discipline_Upload.sql

--- PCSB/Monthly_Discipline_Upload.sql
+++ PCSB/Monthly_Discipline_Upload.sql
@@ -1,5 +1,5 @@
 SELECT
-I.INCIDENTID,
+I.INCIDENTID AS INCIDENTID_REMOVE,
 S.FULLNAME AS NAME_REMOVE,
 'Disciplinary' AS 'Collection Type',
 '2016-2017' AS 'School Year',
@@ -19,6 +19,7 @@ CASE
     WHEN INFRACTION = 'Theft' THEN (CASE WHEN (INJURYTYPE IS NULL or INJURYTYPE = 'No Injury') THEN 'Theft without violence' ELSE 'Robbery' END) --no injury theft without violence, if injury then robbery
 	WHEN INFRACTION = 'Threatening Physical Harm' THEN 'Threat/intimidation'
 	WHEN INFRACTION LIKE 'Weapons%' THEN 'Weapons Possession'
+	WHEN INFRACTION = 'Physical Altercation' THEN (CASE WHEN INJURYTYPE = 'Serious Injury' THEN 'Physical Attack or Harm' WHEN INJURYTYPE IS NULL THEN '***PHYSICAL ALTERCATION TYPE UNKNOWN***' ELSE 'Physical Altercaltion, Minor' END)
 	WHEN INFRACTION IS NULL THEN '**MISSING INFRACTION**'
 	ELSE INFRACTION
 END AS 'Reason Student Was Disciplined', --transform to match 
@@ -27,6 +28,7 @@ CASE
 	WHEN INFRACTION IN ('Bullying','Fighting',
 						'Sexual Misconduct or Harrassment',
 						'Theft','Threatening Physical Harm',
+						'Physical Altercation',
 						'Violent Incident (WITH physical injury) (VIOWINJ)') THEN (CASE WHEN INJURYTYPE IS NULL THEN '**MISSING INJURY TYPE**' ELSE INJURYTYPE END)--'**Choose Minor Injury, Serious Bodily Injury or No Injury**'
 END AS 'Injury Type',
 CASE
@@ -62,6 +64,6 @@ JOIN [CUSTOM].[CUSTOM_DLPENALTIES_RAW] P ON P.INCIDENTID = I.INCIDENTID
 JOIN POWERSCHOOL.POWERSCHOOL_STUDENTS PS ON PS.STUDENT_NUMBER = SB.STUDENT_NUMBER
 JOIN POWERSCHOOL.POWERSCHOOL_SCHOOLS SC ON SC.SCHOOL_NUMBER = PS.SCHOOLID
 WHERE 1=1
-AND coalesce(p.startdate,i.issueTS) BETWEEN '2016-11-01' and '2016-11-30'--CONVERT(DATE,DATEADD(MM, DATEDIFF(MM, 0, GETDATE())-2, 0)) AND CONVERT(DATE,DATEADD(MS, -3, DATEADD(MM, DATEDIFF(MM, 0, GETDATE()) , 0)))
+AND coalesce(p.startdate,i.issueTS) BETWEEN '2017-01-01' and '2017-01-31'--CONVERT(DATE,DATEADD(MM, DATEDIFF(MM, 0, GETDATE())-2, 0)) AND CONVERT(DATE,DATEADD(MS, -3, DATEADD(MM, DATEDIFF(MM, 0, GETDATE()) , 0)))
 AND P.PENALTYNAME IN ('OSS','Expulsion')--,'ISS')
 order by p.startdate
